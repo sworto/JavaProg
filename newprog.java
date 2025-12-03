91eedff72c554ccce0d1264dae34888a8efa14bb
Shivam <shivamdubey252005@outlook.com>
2025-12-03 22:11:43 +0530

    abc

--- newprog.java
+++
@@ -1,9 +0,0 @@
-public class newprog
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Roll No: "+"2405902");
-		System.out.println("Name: "+"Shivam Dubey");
-		System.out.println("Section: "+"CSE45");
-	}
-}
